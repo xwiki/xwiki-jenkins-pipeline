dea4fd6d69f31ae5e08389b59798f43d318e96c1
Vincent Massol <vincent@massol.net>
2018-11-15 10:45:30 +0100

    [Misc] Document future directions

--- scripts/docker-supported.groovy
+++ scripts/docker-supported.groovy
@@ -24,6 +24,18 @@
 // - Browsers: https://dev.xwiki.org/xwiki/bin/view/Community/BrowserSupportStrategy
 // Note that for browsers we're constrained to use the version of them supported by the Selenium version we use. Our
 // strategy is to always use the latest released Selenium version in order to use the latest browser versions.
+//
+// TODO: In the future replace this by Java code located in xwiki-platform-test-docker whe JUnit5 supports this.
+// It'll bring the following advantages:
+// - Less reliance on the CI. If we need to get away from Jenkins for ex, it'll make it easier. In general we need to
+//   have the maximum done in the Maven build and the minimum in CI scripts.
+// - Ability to run several configs at once on developer's machines.
+// - Ability to have a single Maven build executed in the CI and thus not get tons of mails whenever a test fails
+//   (which is the current situation).
+// Disadvantages:
+// - Ability to paralellize, i.e. execute each Maven build on a different CI agent. This is because the withMaven() step
+//   doesn't currently support this feature (which is a pity).
+//   See also https://massol.myxwiki.org/xwiki/bin/view/Blog/Jenkins%20and%20large%20Maven%20projects
 def configurations = [
     'MySQL 5.7.x, Tomcat 8.x, Chrome': [
         'database' : 'mysql',
