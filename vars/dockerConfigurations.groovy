c06a9efb73467017e3b33768c76ecb8080cfa1eb
Vincent Massol <vincent@massol.net>
2019-04-08 11:15:45 +0200

    [Misc] Add documentation

--- vars/dockerConfigurations.groovy
+++ vars/dockerConfigurations.groovy
@@ -30,37 +30,7 @@ def call(configurationName, xwikiPlatformBranch)
 }
 
 /**
- * Defines the latest versions of supported XWiki configurations, according to:
- * <ul>
- * <li>DBs: https://dev.xwiki.org/xwiki/bin/view/Community/DatabaseSupportStrategy</li>
- * <li>Servlet containers: https://dev.xwiki.org/xwiki/bin/view/Community/ServletContainerSupportStrategy/</li>
- * <li>Browsers: https://dev.xwiki.org/xwiki/bin/view/Community/BrowserSupportStrategy</li>
- * </ul>
- * <p>
- * Note that for browsers we're constrained to use the version of them supported by the Selenium version we use. Our
- * strategy is to always use the latest released Selenium version in order to use the latest browser versions.
- * <p>
- * TODO: In the future replace this by Java code located in xwiki-platform-test-docker when JUnit5 supports this.
- * (see https://github.com/junit-team/junit5/issues/871).
- * It'll bring the following advantages:
- * <ul>
- * <li>Less reliance on the CI. If we need to get away from Jenkins for ex, it'll make it easier. In general we need to
- *   have the maximum done in the Maven build and the minimum in CI scripts.</li>
- * <li>Ability to run several configs at once on developer's machines.</li>
- * <li>Ability to have a single Maven build executed in the CI and thus not get tons of mails whenever a test fails
- *   (which is the current situation).</li>
- * <li>Ability to have different configurations depending on the branch in the source (for example in XWiki 10.11.x we
- *   don't support running on Java 11.x</li>
- * </ul>
- * Disadvantages:
- * <ul>
- * <li>Ability to paralellize, i.e. execute each Maven build on a different CI agent. This is because the withMaven()
- *   step doesn't currently support this feature (which is a pity). See also
- *   https://massol.myxwiki.org/xwiki/bin/view/Blog/Jenkins%20and%20large%20Maven%20projects<li>
- * </ul>
- * <p>
- *
- * @param xwikiPlatformBranch the branch to build on (e.g. "stable-10.10.x"). If not specified then we assume it's "master"
+ * Defines the latest versions of supported XWiki configurations.
  */
 def getLatestConfigurations(def xwikiPlatformBranch)
 {
@@ -96,8 +66,6 @@ def getLatestConfigurations(def xwikiPlatformBranch)
 /**
  * Configurations for smoke tests (i.e. only a few tests) on the maximum number of configurations to flush out problems
  * of configurations when XWiki doesn't start or has basic problems. This includes all supported configurations.
- *
- * @param xwikiPlatformBranch the branch to build on (e.g. "master", "stable-10.10.x")
  */
 def getAllConfigurations(def xwikiPlatformBranch)
 {
@@ -186,8 +154,6 @@ def getAllConfigurations(def xwikiPlatformBranch)
 /**
  * Coonfigurations for smoke tests (i.e. only a few tests) on configurations that we'll want to support in the future
  * but that are currently not supported or not working.
- *
- * @param xwikiPlatformBranch the branch to build on (e.g. "master", "stable-10.10.x")
  */
 def getUnsupportedConfigurations(def xwikiPlatformBranch)
 {
