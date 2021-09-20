c80a83694b662160d97a0d514b3558d9e088604a
Vincent Massol <vincent@massol.net>
2021-09-20 10:30:10 +0200

    [Misc] Add false positive recognition

--- vars/checkForFalsePositives.groovy
+++ vars/checkForFalsePositives.groovy
@@ -74,8 +74,9 @@ def call()
     def messages = [
         [".*Error setting up the XWiki testing environment on agent.*", "Docker test setup issue"],
         [".*Java heap space.*", "Memory issue"],
-        [".*Connection refused (Connection refused).*", "Communication issue"],
-        [".*Reached error page: about:neterror.*", "Communication issue"]
+        [".*Connection refused.*", "Communication issue"],
+        [".*Reached error page: about:neterror.*", "Communication issue"],
+        [".*ContainerLaunchException: Timed out waiting for URL to be accessible.*"]
     ]
     messages.each { message ->
         if (manager.logContains(message.get(0))) {
