3d5d2035a0a6132afd88bccd84fb1a3530d80bfa
Vincent Massol <vincent@massol.net>
2024-01-19 11:17:57 +0100

    [Misc] Upgrade to MySQL 8.3 for latest

--- vars/dockerConfigurations.groovy
+++ vars/dockerConfigurations.groovy
@@ -35,7 +35,7 @@ def call(configurationName, xwikiVersion)
     // - we always test with the latest bugfix version
     // - we reduce the maintenance (since specifying the bugfix part would mean updating them all the time)
     def versions = [
-        'mysql' : [ 'latest' : '8.2', 'lts' : '8.0' ],
+        'mysql' : [ 'latest' : '8.3', 'lts' : '8.0' ],
         'mariadb' : [ 'latest' : '11.2', 'lts' : '10.11' ],
         // Note: for postgreSQL latest is the last cycle and LTS the previous one. Thus, we don't specify the minor to
         // be always up to date in our tests.
